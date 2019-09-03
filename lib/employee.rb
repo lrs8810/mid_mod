e3af5a244834243f131d9ec16b4f9e1ab5b0238e
Laura Schulz <lrs8810@gmail.com>
2019-09-02 20:56:16 -0600

    Add completed iteration 1

---
+++ lib/employee.rb
@@ -0,0 +1,11 @@
+class Employee
+  attr_reader :name,
+              :age,
+              :salary
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @age = attributes[:age]
+    @salary = attributes[:salary]
+  end
+end
