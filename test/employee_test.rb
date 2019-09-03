28b8be0c4f5149238d4023be17612a33760cb2a4
Laura Schulz <lrs8810@gmail.com>
2019-09-03 14:42:41 -0600

    Fix employee test

--- test/employee_test.rb
+++ test/employee_test.rb
@@ -3,18 +3,15 @@ require 'minitest/pride'
 require './lib/employee'
 
 class EmployeeTest < Minitest::Test
-
-  def setup
-    @bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
-  end
-
   def test_it_exists
-    assert_instance_of Employee, @bobbi
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    assert_instance_of Employee, bobbi
   end
 
   def test_it_has_attributes
-    assert_equal "Bobbi Jaeger", @bobbi.name
-    assert_equal "30", @bobbi.age
-    assert_equal "100000", @bobbi.salary
+    bobbi = Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})
+    assert_equal "Bobbi Jaeger", bobbi.name
+    assert_equal "30", bobbi.age
+    assert_equal "100000", bobbi.salary
   end
 end
